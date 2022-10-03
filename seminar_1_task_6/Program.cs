5d80c320e6d46d4fd0ebe4a65c40e95081e951b5
= <me@local.dom>
2022-10-03 21:42:16 +0300

    Seminar 1. Task 6. Working code.

---
+++ seminar_1_task_6/Program.cs
@@ -0,0 +1,14 @@
+﻿//Seminar_1_task_6
+//
+
+Console.Write("Введите число: ");
+int x = Convert.ToInt32(Console.ReadLine());
+
+if (x % 2 == 0)
+{
+    Console.WriteLine("Число четное!");
+}
+else
+{
+    Console.WriteLine("Число нечетное!");
+}
