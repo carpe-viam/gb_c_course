d53051e9b50b9341ac501444e3563af1fc4e2314
= <me@local.dom>
2022-10-03 23:01:12 +0300

    Seminar 1. Task 8. Working code.

---
+++ seminar_1_task_8/Program.cs
@@ -0,0 +1,13 @@
+﻿//Seminar_1_task_8
+//
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 2; i <= N; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i + " ");
+    }
+}
