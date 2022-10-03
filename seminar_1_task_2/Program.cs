2144def2ff9f4939d37ae901a242738300ea387f
= <me@local.dom>
2022-10-03 20:27:45 +0300

    Seminar 1. Task 2. Working code

--- seminar_1_task_2/Program.cs
+++ seminar_1_task_2/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Seminar_1_task_2
+//
+Console.Clear();
+Console.Write("Введите первое число: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+
+if (x1 < x2)
+{
+    Console.WriteLine("Второе число больше!");
+}
+else if (x1 > x2)
+{
+    Console.WriteLine("Первое число больше!");
+}
+else
+{
+    Console.WriteLine("Числа равны!");
+}
