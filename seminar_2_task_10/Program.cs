5d7f5ba7097ba49a4f1d1b6eb50ed1d77035e437
= <me@local.dom>
2022-10-07 20:56:11 +0300

    Seminar 2. Task 10

---
+++ seminar_2_task_10/Program.cs
@@ -0,0 +1,26 @@
+﻿//Seminar 2. Task 10
+//
+
+Console.Clear();
+
+Console.Write("Введите трехзначное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+//validation
+while (true)
+{
+    if (n < 0)
+        n = (-1) * n;
+
+    if (n / 1000 < 1 && n / 100 >= 1)
+    {
+        break;
+    }
+    else
+    {
+        Console.Write("Введите ТРЕХЗНАЧНОЕ! число: ");
+        n = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+Console.Write($"Вторая цифра введенного числа: {n / 10 - ((n / 100) * 10)}");
