c53b44948274fd7bff0eb9e8c01e512a2fea9410
= <me@local.dom>
2022-10-07 20:57:22 +0300

    Seminar 2. Task 15

---
+++ seminar_2_task_15/Program.cs
@@ -0,0 +1,13 @@
+﻿//Seminar 2. Task 15
+//
+
+Console.Clear();
+Console.Write("Введите номер дня недели (от 1 до 7): ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n == 6 || n == 7)
+    Console.Write("Это выходной день :)");
+else if (n >= 1 && n <= 6)
+    Console.Write("Это будний день :(");
+else
+    Console.Write("Введенный номер не соответствует дню недели!");
