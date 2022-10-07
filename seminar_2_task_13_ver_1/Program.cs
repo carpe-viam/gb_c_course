51b3280443e8ab5062bd26a9653da997ccbb778f
= <me@local.dom>
2022-10-07 20:56:37 +0300

    Seminar 2. Task 13.1

---
+++ seminar_2_task_13_ver_1/Program.cs
@@ -0,0 +1,17 @@
+﻿//Seminar 2. Task 13 ver. 1
+//
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n < 0)
+    n = (-1) * n;
+
+if (n / 100 == 0)
+    Console.Write("Введенное число не содержит третьей цифры!");
+else
+{
+    string nStr = Convert.ToString(n);
+    Console.WriteLine($"Третья цифра введенного числа: {nStr[2]}");
+}
