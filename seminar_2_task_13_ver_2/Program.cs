eb58f27d0eb03f60c6b1bb6a9376dcf050ea88af
= <me@local.dom>
2022-10-07 20:57:07 +0300

    Seminar 2. Task 13.2

---
+++ seminar_2_task_13_ver_2/Program.cs
@@ -0,0 +1,35 @@
+﻿//Seminar 2. Task 13 ver. 2
+//
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n < 0)
+    n = (-1) * n;
+
+if (n / 100 == 0)
+    Console.Write("Введенное число не содержит третьей цифры!");
+else
+{
+    int count = 0;
+    int x = n;
+
+    // узнаем кол-во разрядов во введенном числе
+    while (x > 0)
+    {
+        x = x / 10;
+        count++;
+    }
+
+    int[] numbers = new int[count];
+    int ind = 0;
+    while (ind < count)
+    {
+        numbers[ind] = n % 10;
+        n = n / 10;
+        ind++;
+    }
+
+    Console.WriteLine($"Третья цифра: {numbers[count-3]}");
+}
